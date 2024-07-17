6007921f6b18ce09ac087bbb84248298af747338
mt8 <info@mt8.biz>
2024-07-17 19:41:45 +0900

    fix use @wordpress/e2e-test-utils-playwright

--- tests/playwright/hello.spec.js
+++ tests/playwright/hello.spec.js
@@ -1,25 +1,13 @@
 
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test('Check if Hello Dolly lyrics are displayed on the dashboard', async ({ page }) => {
-  // create admin url
-  const wpEnv = require('../../.wp-env.json');
-  let port = wpEnv.port ? wpEnv.port : 8888;
-  try {
-    const wpEnvOverride = require('../../.wp-env.override.json');
-    Object.assign(wpEnv, wpEnvOverride);
-    port = wpEnv.port ? wpEnv.port : 8888;
-  } catch (e) {
-    // do nothing
-  }
-  // log in to the admin dashboard
-  await page.goto(`http://localhost:${port}/wp-admin/`);
-  await page.fill('#user_login', 'admin');
-  await page.fill('#user_pass', 'password');
-  await page.click('#wp-submit');
-
-  // check if Hello Dolly is displayed
-  const helloDolly = await page.innerText('#dolly');
-  expect(helloDolly).not.toBe('');
-
-});
+test.describe( 'Hello Dolly', () => {
+	test( 'Check if Hello Dolly lyrics are displayed on the dashboard', async ( {
+		page,
+		admin,
+	} ) => {
+		await admin.visitAdminPage( '/' );
+    const helloDolly = await page.innerText('#dolly');
+    expect(helloDolly).not.toBe('');  
+	} );
+} );
